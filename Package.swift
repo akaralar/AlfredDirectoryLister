f2767050098b3aa321cac81ccf587dc076c67570
Ahmet Karalar <akaralar@gmail.com>
2024-04-30 19:46:31 +0200

    Change to local dependency

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.0"),
-        .package(name: "AlfredJSONEncoder", path: "AlfredJSONEncoder")
+        .package(name: "AlfredJSONEncoder", path: "../AlfredJSONEncoder")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
