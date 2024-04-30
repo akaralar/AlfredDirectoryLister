c62c2746355fbfa7ea3a6eed21cb7d2f6c52a230
Ahmet Karalar <akaralar@gmail.com>
2024-04-30 19:49:14 +0200

    Add and implement all input arguments

--- Sources/DirectoryLister/DirectoryLister.swift
+++ Sources/DirectoryLister/DirectoryLister.swift
@@ -7,30 +7,158 @@
 import Foundation
 import ArgumentParser
 import AlfredJSONEncoder
+import RegexBuilder
 
 @main
 struct ListDirectoryMain: ParsableCommand {
 
     @Option(
         name: [.short, .customLong("dir")],
-        help: "Directory to list",
-        transform: URL.init(string:)
+        help: "Directory to list"
     )
-    var directory: URL?
+    var directory: String = "~/Downloads"
 
     @Option(
         name: [.short, .long],
-        help: "Maximum items to return"
+        help: "Query for fuzzy matching of filenames"
+    )
+    var query: String?
+
+    @Option(
+        name: [.short, .customLong("max")],
+        help: "Maximum items to return. If zero, all items will be returned"
     )
     var maxItems: Int = 0
 
     @Flag(
         name: [.customShort("i"), .customLong("include-dir")],
-        help: "Flag to denote if the input directory is returned as the first item in the list"
+        help: "Returns the input directory as the first item in the list"
     )
     var shouldIncludeInputDirectory: Bool = false
 
+    @Flag(
+        name: [.customShort("c"), .customLong("ignore-case")],
+        help: "Case-insensitive matching of query"
+    )
+    var shouldIgnoreCase: Bool = false
+
     mutating func run() throws {
-        print(try AlfredListItem.dummy.toJSON())
+        let fm = FileManager.default
+        
+        let url = if directory.starts(with: "~") {
+            URL(fileURLWithPath: directory.expandingTilde(), isDirectory: true)
+        } else if directory.starts(with: "/") {
+            URL(fileURLWithPath: directory, isDirectory: true)
+        } else {
+            FileManager.default.homeDirectoryForCurrentUser.appending(path: directory)
+        }
+
+        var isDir: ObjCBool = false
+        if fm.fileExists(atPath: url.path(), isDirectory: &isDir), isDir.boolValue {
+            var sortedFiles = files(in: url, matching: query, maxItems: maxItems)
+            if shouldIncludeInputDirectory {
+                sortedFiles.insert(File(url: url, name: directory, addedDate: Date()), at: 0)
+            }
+            let alfredItems = AlfredList(items: sortedFiles.map(AlfredListItem.init(file:)))
+            print(try alfredItems.toJSON())
+        } else {
+            print("Directory not found at given path.")
+        }
+    }
+
+    func files(in url: URL, matching query: String?, maxItems: Int = 0) -> [File] {
+        let resourceKeys = Set<URLResourceKey>([.nameKey, .addedToDirectoryDateKey])
+        let fm = FileManager.default
+        let directoryEnumerator = fm.enumerator(
+            at: url,
+            includingPropertiesForKeys: Array(resourceKeys),
+            options: [.skipsPackageDescendants, .skipsSubdirectoryDescendants]
+        )!
+
+        var files: [File] = []
+        for case let fileURL as URL in directoryEnumerator {
+            guard let resourceValues = try? fileURL.resourceValues(forKeys: resourceKeys),
+                  let name = resourceValues.name,
+                  let addedDate = resourceValues.addedToDirectoryDate
+            else {
+                print("Can't extract resource values for \(fileURL)")
+                continue
+            }
+
+            let file = File(url: fileURL, name: name, addedDate: addedDate)
+            guard let query = query else {
+                files.append(file)
+                continue
+            }
+
+            let (q, n) = if shouldIgnoreCase {
+                (query.lowercased(), name.lowercased())
+            } else {
+                (query, name)
+            }
+
+            guard n.firstMatch(of: regexPattern(for: q)) != nil else { continue }
+            files.append(file)
+        }
+
+        let sorted = files.sorted { $0.addedDate > $1.addedDate }
+        return maxItems == 0 ? sorted : Array(sorted.prefix(maxItems))
+    }
+
+    private func regexPattern(for query: String) -> some RegexComponent {
+        let fuzzyPattern = query.map { "\($0).*" }.joined()
+        return try! Regex(fuzzyPattern)
+    }
+}
+
+struct File {
+    var path: String
+    var name: String
+    var addedDate: Date
+
+    init(url: URL, name: String, addedDate: Date) {
+        self.path = url.path(percentEncoded: false)
+        self.name = name
+        self.addedDate = addedDate
+    }
+}
+
+extension String {
+    func expandingTilde() -> String {
+        if starts(with: "~/") {
+            return replacingOccurrences(
+                of: "~",
+                with: FileManager.default.homeDirectoryForCurrentUser.path().dropLast()
+            ).removingPercentEncoding!
+
+        } else if starts(with: "~") {
+            return replacingOccurrences(
+                of: "~",
+                with: FileManager.default.homeDirectoryForCurrentUser.path()
+            ).removingPercentEncoding!
+        } else {
+            return self
+        }
+    }
+}
+
+extension AlfredListItem {
+    init(file: File) {
+        self.init(
+            uid: nil,
+            title: file.name,
+            subtitle: file.path,
+            arguments: .single(file.path),
+            icon: Icon(path: file.path, type: .fileIcon),
+            isValid: true,
+            match: nil,
+            autocomplete: file.name,
+            type: .skipCheck,
+            modifierActions: nil,
+            action: .universalAction(.file(file.path)),
+            text: Text(copy: file.path, largeType: file.name),
+            quicklookURL: file.path,
+            skipKnowledge: true
+        )
     }
 }
