7e6d56de72c5be018f5c17fe3510b62349cf6186
Ahmet Karalar <akaralar@gmail.com>
2024-05-01 20:53:47 +0200

    Remove comments in package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -18,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/akaralar/AlfredJSONEncoder", from: "1.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "DirectoryLister",
             dependencies: [
