04bac340293860c592e4b0de154dc7fe4d1e5d3b
Ahmet Karalar <akaralar@gmail.com>
2024-05-01 18:25:01 +0200

    Replace if with guard

--- Sources/DirectoryLister/DirectoryLister.swift
+++ Sources/DirectoryLister/DirectoryLister.swift
@@ -7,7 +7,6 @@
 import Foundation
 import ArgumentParser
 import AlfredJSONEncoder
-import RegexBuilder
 
 @main
 struct ListDirectoryMain: ParsableCommand {
@@ -43,24 +42,24 @@ struct ListDirectoryMain: ParsableCommand {
     var caseInsensitive: Bool = false
 
     mutating func run() throws {
-        if let inputDir = InputDirectory(directory: directory) {
-            let sortedFiles = inputDir.files(matching: query, caseInsensitive: caseInsensitive, maxItems: maxItems)
-            var alfredList = if sortedFiles.isEmpty {
-                AlfredList(items: [Error(text: "No results matching input")])
-            } else {
-                AlfredList(items: sortedFiles)
-            }
-
-            if shouldIncludeInputDirectory {
-                alfredList.items.insert(File(inputDir: inputDir).asAlfredListItem, at: 0)
-            }
-
-            print(try alfredList.toJSON())
-        }
-        else {
+        guard let inputDir = InputDirectory(directory: directory) else {
             let list = AlfredList(items: [Error(text: "Directory not found at given path.")])
             print(try list.toJSON())
+            return
         }
+
+        let sortedFiles = inputDir.files(matching: query, caseInsensitive: caseInsensitive, maxItems: maxItems)
+        var alfredList = if sortedFiles.isEmpty {
+            AlfredList(items: [Error(text: "No results matching input")])
+        } else {
+            AlfredList(items: sortedFiles)
+        }
+
+        if shouldIncludeInputDirectory {
+            alfredList.items.insert(File(inputDir: inputDir).asAlfredListItem, at: 0)
+        }
+        
+        print(try alfredList.toJSON())
     }
 }
 
